cdcddc7c7118c0a161a4661a19f782c6d25bccb3
Nancuante-Juan <Una_oveja@proton.me>
2025-10-15 22:42:25 -0300

    no se

--- primera_instalacion.bat
+++ primera_instalacion.bat
@@ -2,7 +2,7 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 timeout /t 1
 winget install Python.PythonInstallManager
 timeout /t 3
-py install 3.10
+pymanager install 3.10
 timeout /t 3
 py -3.10 -m venv venv310
 timeout /t 3
