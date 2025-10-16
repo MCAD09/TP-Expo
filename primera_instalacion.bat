91062ca3117a31147f1b6b1e8138c627e9843302
Nancuante-Juan <Una_oveja@proton.me>
2025-10-15 23:02:02 -0300

    version definitiva del script

--- primera_instalacion.bat
+++ primera_instalacion.bat
@@ -1,12 +1,40 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-timeout /t 1
 winget install Python.PythonInstallManager
 timeout /t 5
 pymanager install 3.10
-timeout /t 1
 py -3.10 -m venv venv310
-timeout /t 1
-.\venv310\Scripts\activate
-timeout /t 1
-pip install mediapipe opencv-python pygame
-timeout /t 10
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+echo Copia esta linea y pegala debajo:  `pip install mediapipe opencv-python pygame`
+cmd /k .\venv310\Scripts\activate 
