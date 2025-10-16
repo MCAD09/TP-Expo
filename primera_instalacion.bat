afb4a392c4d114bf33f85f87b4afc9066767e437
Nancuante-Juan <Una_oveja@proton.me>
2025-10-15 22:38:58 -0300

    se redujo el teimpo

--- primera_instalacion.bat
+++ primera_instalacion.bat
@@ -1,10 +1,11 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+timeout /t 1
 winget install Python.PythonInstallManager
-timeout /t 5
+timeout /t 3
 py install 3.10
-timeout /t 5
+timeout /t 3
 py -3.10 -m venv venv310
-timeout /t 5
+timeout /t 3
 .\venv310\Scripts\activate
-timeout /t 5
+timeout /t 3
 pip install mediapipe opencv-python pygame
