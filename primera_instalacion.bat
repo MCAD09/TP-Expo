a39d53fac0715ab0507edfc81057cdf3a22e74fd
Nancuante-Juan <Una_oveja@proton.me>
2025-10-15 22:44:22 -0300

    hjkjahsdkjfhjkasdhkjfal

--- primera_instalacion.bat
+++ primera_instalacion.bat
@@ -1,11 +1,12 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 timeout /t 1
 winget install Python.PythonInstallManager
-timeout /t 3
+timeout /t 5
 pymanager install 3.10
-timeout /t 3
+timeout /t 1
 py -3.10 -m venv venv310
-timeout /t 3
+timeout /t 1
 .\venv310\Scripts\activate
-timeout /t 3
+timeout /t 1
 pip install mediapipe opencv-python pygame
+timeout /t 10
